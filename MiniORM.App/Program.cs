69a64ffe0c828a6c8f6c462435c1a59ccf900d83
betsovIT <47607509+betsovIT@users.noreply.github.com>
2019-10-29 12:30:21 +0200

    Done with the damned thing...

---
+++ MiniORM.App/Program.cs
@@ -0,0 +1,25 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+using System;
+using System.Linq;
+
+namespace MiniORM.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new SoftUniDbContext(@"server=DESKTOP-AGCLSI5\SQLEXPRESS;database=MiniORM;trusted_connection=true;");
+
+            db.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = 1,
+                IsEmployed = true
+            });
+
+            db.SaveChanges();
+        }
+    }
+}
