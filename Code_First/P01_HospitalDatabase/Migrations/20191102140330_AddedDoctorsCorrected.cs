5624e1a730ce9eeac57e611aa82297dbaccae7ac
betsovIT <47607509+betsovIT@users.noreply.github.com>
2019-11-02 19:34:08 +0200

    Solved all of the exercises

---
+++ Code_First/P01_HospitalDatabase/Migrations/20191102140330_AddedDoctorsCorrected.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P01_HospitalDatabase.Migrations
+{
+    public partial class AddedDoctorsCorrected : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Speciality",
+                table: "Doctors",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Doctors",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Speciality",
+                table: "Doctors",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Doctors",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
---
+++ Code_First/P03_SalesDatabase/Migrations/20191102111907_ProductsAddColumnDescription.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P03_SalesDatabase.Migrations
+{
+    public partial class ProductsAddColumnDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                maxLength: 250,
+                nullable: true,
+                defaultValue: "No description",
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 250,
+                oldNullable: true,
+                oldDefaultValue: "No description");
+        }
+    }
+}
