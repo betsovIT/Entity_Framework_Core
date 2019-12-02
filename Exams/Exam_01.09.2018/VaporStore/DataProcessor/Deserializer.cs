72fa8cf9507b5d148c0912b07666957179889909
betsovIT <47607509+betsovIT@users.noreply.github.com>
2019-12-02 16:52:45 +0200

    Solved one exam entirely, other one is partialy solved.

---
+++ Exams/Exam_01.09.2018/VaporStore/DataProcessor/Deserializer.cs
@@ -0,0 +1,33 @@
+﻿namespace VaporStore.DataProcessor
+{
+	using System;
+    using System.ComponentModel.DataAnnotations;
+    using Data;
+    using Newtonsoft.Json;
+    using VaporStore.DataProcessor.ImportDTO;
+
+    public static class Deserializer
+	{
+		public static string ImportGames(VaporStoreDbContext context, string jsonString)
+		{
+            var deserializationResult = JsonConvert.DeserializeObject<GameImportDTO>(jsonString);
+		}
+
+		public static string ImportUsers(VaporStoreDbContext context, string jsonString)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static string ImportPurchases(VaporStoreDbContext context, string xmlString)
+		{
+			throw new NotImplementedException();
+		}
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+
+
+        }
+	}
+}
