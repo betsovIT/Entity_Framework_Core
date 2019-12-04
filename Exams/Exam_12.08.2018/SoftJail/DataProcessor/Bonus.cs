a6f301b43318dc07a76c9a17ed201acbb0b6b8ee
betsovIT <47607509+betsovIT@users.noreply.github.com>
2019-12-04 19:33:03 +0200

    Solved the exam except for one damned problem.

--- Exams/Exam_12.08.2018/SoftJail/DataProcessor/Bonus.cs
+++ Exams/Exam_12.08.2018/SoftJail/DataProcessor/Bonus.cs
@@ -3,12 +3,25 @@
 
     using Data;
     using System;
+    using System.Linq;
 
     public class Bonus
     {
         public static string ReleasePrisoner(SoftJailDbContext context, int prisonerId)
         {
-           throw new NotImplementedException();
+            var prisoner = context.Prisoners.FirstOrDefault(p => p.Id == prisonerId);
+
+            if (prisoner.ReleaseDate == null)
+            {
+                return $"Prisoner {prisoner.FullName} is sentenced to life";
+            }
+            else
+            {
+                prisoner.ReleaseDate = DateTime.Now;
+                context.SaveChanges();
+
+                return $"Prisoner {prisoner.FullName} released";
+            }
         }
     }
 }
